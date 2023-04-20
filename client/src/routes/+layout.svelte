1251dcb3f71d09ac04a11f312e41788881593d0d
ionmi <ionmiromero@gmail.com>
2023-04-20 19:24:01 +0200

    cosas que me habia dejado

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+    import "../app.css";
+</script>
+
+<slot/>
---
+++ client/src/routes/+layout.ts
@@ -0,0 +1,6 @@
+import type { LayoutLoad } from './$types';
+export const ssr = false;
+
+export const load = (async () => {
+    return {};
+}) satisfies LayoutLoad;
